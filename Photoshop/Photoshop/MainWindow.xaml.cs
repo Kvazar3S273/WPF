7234446545913ba2f7e4cd78f6025f21619a974f
Kvazar3S273 <ba2h007@gmail.com>
2021-03-14 20:37:36 +0200

    Create Photoshop

--- Photoshop/Photoshop/MainWindow.xaml.cs
+++ Photoshop/Photoshop/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Ink;
+
 
 namespace Photoshop
 {
@@ -23,6 +26,63 @@ namespace Photoshop
         public MainWindow()
         {
             InitializeComponent();
+            // Установить режим Ink в качестве стандартного.
+            this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            this.inkRadio.IsChecked = true;
+            this.comboColors.SelectedIndex = 0;
+        }
+
+        private void RadioButtonClicked(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as RadioButton)?.Content.ToString())
+            {
+                // Эти строки должны совпадать со значениями свойства Content
+                // каждого элемента RadioButton.
+                case "Ink Mode!":
+                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                    break;
+                case "Erase Mode!":
+                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+                    break;
+                case "Select Mode!":
+                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Select;
+                    break;
+            }
+        }
+
+        private void ColorChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Получить выбранный элемент в раскрывающемся списке.
+            string colorToUse =
+            (this.comboColors.SelectedItem as StackPanel).Tag.ToString();
+            // Изменить цвет, используемый для визуализации штрихов.
+            this.MyInkCanvas.DefaultDrawingAttributes.Color =
+            (Color)ColorConverter.ConvertFromString(colorToUse);
+        }
+
+        private void SaveData(object sender, RoutedEventArgs e)
+        {
+            // Сохранить все данные InkCanvas в локальном файле.
+            using (FileStream fs = new FileStream("StrokeData.bin", FileMode.Create))
+            {
+                this.MyInkCanvas.Strokes.Save(fs);
+                fs.Close();
+            }
+        }
+        private void LoadData(object sender, RoutedEventArgs e)
+        {
+            // Наполнить StrokeCollection из файла.
+            using (FileStream fs = new FileStream("StrokeData.bin",
+            FileMode.Open, FileAccess.Read))
+            {
+                StrokeCollection strokes = new StrokeCollection(fs);
+                this.MyInkCanvas.Strokes = strokes;
+            }
+        }
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            // Очистить все штрихи.
+            this.MyInkCanvas.Strokes.Clear();
         }
     }
 }
