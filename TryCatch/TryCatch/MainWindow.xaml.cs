f2913689bcd9b8d635b93c66466c77ecd23d29e6
Kvazar3S273 <ba2h007@gmail.com>
2021-03-15 23:39:29 +0200

    Create TryCatch example

---
+++ TryCatch/TryCatch/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TryCatch
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			double a = double.Parse(tb.Text);
+			double result;
+			try
+			{
+				result = 12345 / a;
+				MessageBox.Show($"{result}");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Помилка: " + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+			}
+
+		}
+
+	}
+	public partial class App : Application
+	{
+		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+			e.Handled = true;
+		}
+	}
+}
