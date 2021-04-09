2d2a8631c4df3a50558f3e12d5791361100c3dbc
Kvazar3S273 <ba2h007@gmail.com>
2021-04-09 22:41:05 +0300

    Create Threads project

---
+++ Threads/EBookReader/ThreadPoolApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadPoolApp
+{
+    #region Helper class
+    public class Printer
+    {
+        private object lockToken = new object();
+
+        public void PrintNumbers()
+        {
+            lock (lockToken)
+            {
+                // Display Thread info.
+                Console.WriteLine("Потік {0} виконує PrintNumbers()",
+                  Thread.CurrentThread.ManagedThreadId);
+
+                // Print out numbers.
+                Console.Write("Ваші номери: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write("{0}, ", i);
+                    Thread.Sleep(200);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    #endregion
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Основний потік запущено. ThreadID = {0}",
+              Thread.CurrentThread.ManagedThreadId);
+
+            Printer p = new Printer();
+
+            WaitCallback workItem = new WaitCallback(PrintTheNumbers);
+
+            // Queue the method 10 times
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(workItem, p);
+            }
+
+            Console.WriteLine("Усі завдання в черзі");
+            Console.ReadLine();
+        }
+
+        static void PrintTheNumbers(object state)
+        {
+            Printer task = (Printer)state;
+            task.PrintNumbers();
+        }
+    }
+}
